4515709bea375a98ea300bbd11b6f71a10bf51ca
Parmida Vahdatnia <pvahdatn@sfu.ca>
2020-11-24 21:19:17 -0800

    clean up the scala files

--- examples/Machsuite/common.mk
+++
@@ -1,8 +0,0 @@
-LLVM_DIR=/home/parmida/git/muir/dependencies/Tapir-Meta/tapir/build/bin
-CC=$(LLVM_DIR)/clang
-OPT=$(LLVM_DIR)/opt
-DIS=$(LLVM_DIR)/llvm-dis
-
-DANDELION=dandelion
-DANDELION_CONFIG=/home/parmida/git/muir/build/scripts/config.json
-
